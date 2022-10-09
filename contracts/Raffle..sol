cc18c196b9916612b4299168dcf7ca0fd6f3ce5a
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-10-09 11:49:04 +0530

    check upkeep logic added

--- contracts/Raffle..sol
+++ contracts/Raffle..sol
@@ -15,8 +15,14 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
 error Raffle__NotEnoughEthEntered();
 error Raffle__TransferFailed();
+error Raffle__NotOpen();
 
 contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+	/* Type declarations */
+	enum RaffleState {
+		OPEN,
+		CALCULATING
+	}
 	/*State Variables*/
 	uint256 private immutable i_entranceFee;
 	//payable since we need to pay the players if they win
@@ -30,6 +36,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
 
 	//Lottery Variables
 	address private s_recentWinner;
+	RaffleState private s_raffleState;
 
 	/* Events */
 	event RaffleEnter(address indexed player);
@@ -49,11 +56,15 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
 		i_gasLane = gasLane;
 		i_subscriptionId = subscriptionId;
 		i_callbackGasLimit = callbackGasLimit;
+		s_raffleState = RaffleState.OPEN;
 	}
 
 	function enterRaffle() public payable {
 		if (msg.value < i_entranceFee) {
 			revert Raffle__NotEnoughEthEntered();
+		}
+		if (s_raffleState != RaffleState.OPEN) {
+			revert Raffle__NotOpen();
 		}
 		//since, msg.sender is not a payable address we need to typecast it to payable
 		s_players.push(payable(msg.sender));
@@ -65,22 +76,19 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
 	/**
 	 * @dev This is the function that the chainlink keeper nodes call they look for the 'upkeepNeeded' return true
 	 * The following should be true in order to return true:
-	 *
+	 *	1. Our time interval should have passed
+	 *  2. The lottery should have atleast one player and have some eth
+	 * 	3. Our subscription is funded with LINK
+	 * 	4. Lottery should be in open state
 	 */
 
 	function checkUpkeep(
 		bytes calldata /*checkData*/
-	)
-		external
-		override
-		returns (
-			bool upkeepNeeded,
-			bytes memory /* performData */
-		)
-	{}
+	) external override {}
 
 	function requestRandomWinner() external {
 		//two tarnsaction process: 1. request the random number 2. Do something with it
+		s_raffleState = RaffleState.CALCULATING;
 		uint256 requestId = i_vrfCoordinator.requestRandomWords(
 			i_gasLane, //gasLane : the maximum gas price you are willing to pay for a request in wei.
 			i_subscriptionId, //subscription that we need to fund the requests
@@ -99,6 +107,8 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
 		uint256 indexOfWinner = randomWords[0] % s_players.length;
 		address payable recentWinner = s_players[indexOfWinner];
 		s_recentWinner = recentWinner;
+		s_raffleState = RaffleState.OPEN;
+		s_players = new address payable[](0);
 		(bool success, ) = recentWinner.call{value: address(this).balance}("");
 		if (!success) {
 			revert Raffle__TransferFailed();
