142225a051c8316e55c7e121ae58cf23c4e825c3
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-10-12 22:45:25 +0530

    unit tests for enterRaffle

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,4 +1,4 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat");
 const {
 	developmentChains,
@@ -8,17 +8,23 @@ const {
 !developmentChains.includes(network.name)
 	? describe.skip
 	: describe("Raffle", async function () {
-			let raffle, vrfCoordinatorV2Mock;
+			let raffle,
+				vrfCoordinatorV2Mock,
+				raffleEntranceFee,
+				deployer,
+				interval;
 			const chainId = network.config.chainId;
 
 			beforeEach(async function () {
-				const { deployer } = await getNamedAccounts();
+				deployer = (await getNamedAccounts()).deployer;
 				await deployments.fixture(["all"]); //both our raffle and mocks has "all" tags
 				raffle = await ethers.getContract("Raffle", deployer);
 				vrfCoordinatorV2Mock = await ethers.getContract(
 					"VRFCoordinatorV2Mock",
 					deployer
 				);
+				raffleEntranceFee = await raffle.getEntranceFee();
+				interval = await raffle.getInterval();
 			});
 
 			describe("constructor", async function () {
@@ -31,6 +37,48 @@ const {
 						interval.toString(),
 						networkConfig[chainId]["keepersUpdateInterval"]
 					);
+				});
+			});
+
+			describe("enterRaffle", async function () {
+				it("reverts when you don't pay enough", async function () {
+					await expect(raffle.enterRaffle()).to.be.revertedWith(
+						"Raffle__NotEnoughEthEntered"
+					);
+				});
+
+				it("records players when they enter", async function () {
+					await raffle.enterRaffle({ value: raffleEntranceFee });
+					const playerFromContract = await raffle.getPlayer(0);
+					assert.equal(playerFromContract, deployer);
+				});
+
+				it("emits event on enter", async function () {
+					await expect(
+						raffle.enterRaffle({ value: raffleEntranceFee })
+					).to.emit(raffle, "RaffleEnter");
+				});
+
+				it("doesn't allow entrance when raffle is calculating", async function () {
+					//now to go on calculating state we need to perform the upkeep, but performUpkeep can only be called
+					//if checkUpkeep returns true and for that to return true we have
+					//our contract is open && sufficient time has passed && contract has atleast a player registered && contract has balance
+
+					//now for above condition to satisfy we need to pass some time and at the same time some blocks need to be mined
+					//for that hardhat comes built in with a lot of functions for the contract to do what we want it to do (Use hardhat network reference for this)
+					// so we'll use evm_increaseTime && evm_mine for time and mining some block
+
+					await raffle.enterRaffle({ value: raffleEntranceFee });
+					await network.provider.send("evm_increaseTime", [
+						interval.toNumber() + 1,
+					]);
+					await network.provider.send("evm_mine", []);
+
+					//now all the mentioned conditions are satisfied, hence we can pretend to be Chainlink Keeper
+					await raffle.performUpkeep([]); //[] denote empty call data
+					await expect(
+						raffle.enterRaffle({ value: raffleEntranceFee })
+					).to.be.revertedWith("Raffle__NotOpen");
 				});
 			});
 	  });
